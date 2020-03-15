c80f322e59a52499f2b2e99ba314828254750718
Admin <Admin@ADMINRG-PK096SO>
2020-03-15 15:54:35 +0530

    Created Sessionfactory Registry

--- TablePerClassHierarchy/src/com/tpch/utility/SessionFactoryRegistry.java
+++ TablePerClassHierarchy/src/com/tpch/utility/SessionFactoryRegistry.java
@@ -1,5 +1,32 @@
 package com.tpch.utility;
 
-public class SessionFactoryRegistry {
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+public class SessionFactoryRegistry {
+	private static SessionFactory mysqlsessionFactory;
+	static
+	{
+		try
+		{
+			mysqlsessionFactory=new MetadataSources(new StandardServiceRegistryBuilder().configure().build()).buildMetadata().buildSessionFactory();
+		}
+		catch (Throwable t)
+		{
+			t.printStackTrace();
+			throw t;
+		}
+	}
+	public static final SessionFactory getMysqlsessionFactory()
+	{
+		return mysqlsessionFactory;
+	}
+	public static void closesessionFactory()
+	{
+		if(mysqlsessionFactory!=null)
+		{
+			mysqlsessionFactory.close();
+		}
+	}
 }
